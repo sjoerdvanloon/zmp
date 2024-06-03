ed4a7f59429dfd1288d398963b6de407c13d4827
Sjoerd van Loon <sjoerdvanloon83@gmail.com>
2024-06-03 19:56:27 +0200

    Revert "Revert "SSR component""
    This reverts commit 018be2ca78fc49a243a9c2e949778c5c95e0e128.

---
+++ components/base/Button.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: "alert",
+  props: ["foo", "type"],
+  emits: ["plick"],
+  methods: {
+    clickerdyclick() {
+      this.$emit("plick");
+    },
+  },
+};
+</script>
+<template>
+  <button @click="clickerdyclick">{{ foo }}</button>
+</template>
